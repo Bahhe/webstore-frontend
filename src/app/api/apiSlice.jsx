ee44fc81385606f32371c0c079632c2b5e7f21b8
bahadine <marchelldteach@gmail.com>
2023-01-19 20:16:33 +0100

    changes

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { setCredentials } from "../../features/auth/authSlice"
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://timgad-api.onredner.com",
+  baseUrl: "https://timgad-api.onrendner.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
