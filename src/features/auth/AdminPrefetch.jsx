a89d67b2d05fa77f7c2e85a7fac20429a88e99ca
bahadine <marchelldteach@gmail.com>
2023-01-29 02:17:26 +0100

    changed base url

--- src/features/auth/AdminPrefetch.jsx
+++ src/features/auth/AdminPrefetch.jsx
@@ -1,6 +1,6 @@
 import { store } from "../../app/store"
 import { productsApiSlice } from "../products/productsApiSlice"
-import { usersApiSlice } from "../users/usersApiSlice"
+// import { usersApiSlice } from "../users/usersApiSlice"
 import { ordersApiSlice } from "../orders/ordersApiSlice"
 import { useEffect } from "react"
 import { Outlet } from "react-router-dom"
@@ -12,9 +12,9 @@ const Prefetch = () => {
         force: true,
       })
     )
-    store.dispatch(
-      usersApiSlice.util.prefetch("getUsers", "users", { force: true })
-    )
+    // store.dispatch(
+    //   usersApiSlice.util.prefetch("getUsers", "users", { force: true })
+    // )
     store.dispatch(
       ordersApiSlice.util.prefetch("getOrders", "orders", { force: true })
     )
